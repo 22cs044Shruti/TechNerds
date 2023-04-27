df5fea84cf6f6f047d6cd215b78c44984853d8e3
22cs044Shruti <22cs044@charusat.edu.in>
2023-04-27 09:20:47 +0530

    commit

--- project.cpp
+++ project.cpp
@@ -7,180 +7,178 @@ using namespace std;
 class Bank_Account{
 public:
 string Birthdate,Address,gender,status,Email_add,Qualification,Occupation;
+string NameAccHolder,AccType;
+long int First_deposit_money,Current_Deposit_money,Withdraw_money,Bank_balance=0;
 int Age;
 char AdharNum[12],MobileNumb[10],Pancard_Num[10];
-    void To_create_acc();
-    // long long int AccNum;
-    void Deposit();
-    void To_Withdrawl();
-};
 long long int AccNum;
-string NameAccHolder,AccType;
-long int First_deposit_money,Current_Deposit_money,Withdrawl_money,Bank_balance=0;
-void Show_Acc();
-
+void To_create_acc();
+void Show_Acc(long long int);
+void deposit(long long int);
+void To_Withdrawl(long long int);
+void Print_Pattern();
+};
 void Bank_Account::To_create_acc(){
-cout<<"Hello..."<<endl
-    <<"....Welcome To DSD Bank...."<<endl
-    <<"Enter: "<<endl;
-cout<<"Name of account holder: ";
-cin>>NameAccHolder;
-cout<<"Date of birth: ";
+
+cout<<"Fill up the details to create a new account:"<<endl;
+cout<<"Which type of account you want to create??"<<endl
+    <<"1).Saving Account 2).Current Account"<<endl;
+int n;
+cin>>n;
+switch(n)
+{
+case 1:
+    cout<<"Creating Saving Account..."<<endl;;
+    break;
+case 2:
+    cout<<"Creating Current Account..."<<endl;;
+    break;
+default:
+    cout<<"Account type is not valid"<<endl;;
+    break;
+    }
+if(n==1 || n==2){
+ cout<<"Name of account holder: ";
+fflush(stdin);
+getline(cin,NameAccHolder);
+cout<<"Date of birth (DD/MM/YYYY): ";
 cin>>Birthdate;
 cout<<"Age: ";
 cin>>Age;
-cout<<"Gender: ";
+cout<<"Gender ( MALE / FEMALE / OTHER ): ";
 cin>>gender;
-cout<<"Status(NRI/INDIAN): ";
+cout<<"Status ( NRI / INDIAN ): ";
 cin>>status;
 cout<<"Address: ";
-cin>>Address;
+fflush(stdin);
+getline(cin,Address);
 cout<<"Qualification: ";
-cin>>Qualification;
+fflush(stdin);
+getline(cin,Qualification);
 cout<<"Email address: ";
 cin>>Email_add;
-cout<<"Aadhaar Number: ";
+cout<<"* Aadhar Number ( 12 digits ): ";
 cin>>AdharNum;
-cout<<"Mobile Number: ";
+cout<<"Mobile Number ( 10 digits ): ";
 cin>>MobileNumb;
 cout<<"Occupation: ";
-cin>>Occupation;
+fflush(stdin);
+getline(cin,Occupation);
 cout<<"How much amount you want to deposit?? ";
 cin>>First_deposit_money;
-cout<<"Which type of account you want to create??"<<endl<<"1).Saving Account 2).Current Account"<<endl;
-cin>>AccType;
-cout<<"Account is created..."<<endl;
 srand((unsigned) time(0));
-    AccNum = rand() % 1000000 + 100000; // generate a random 6-digit number
-
-    cout << "Account number: " << AccNum << endl;
-    cout << "Account is created..." << endl;
+AccNum = rand() % 1000000 + 100000; // generate a random 6-digit number
+cout << "Account number: " << AccNum << endl;
+cout<<"Account is created..."<<endl;
+Bank_balance=First_deposit_money+Current_Deposit_money;
+}
+else{
+cout<<"Choose valid Account Type"<<endl;
+}
 }
 
-void Show_Acc(){
+void Bank_Account::deposit(long long int x){
+cout<<"How much amount you want to deposit?? ";
+cin>>Current_Deposit_money;
+Bank_balance=First_deposit_money+Current_Deposit_money;
+cout<<"Bank Balance: "<<Bank_balance<<" Rs."<<endl;
+}
+void Bank_Account::Show_Acc(long long int y){
 cout<<"Name: "<<NameAccHolder<<endl
-    <<"Account Type: "<<AccType<<endl
-    <<"Balance: "<<Bank_balance<<endl
+    <<"Account Type: "<<AccType<<endl;
+cout<<"Balance: "<<Bank_balance<<" Rs."<<endl
     <<"Account number: "<<AccNum<<endl;
 }
 
-void Bank_Account::Deposit(){
-   cout<<"How much amount you want to deposit: ";
-   cin>>Current_Deposit_money;
-   First_deposit_money+=Current_Deposit_money;
-   cout<<"Total Balance: "<<First_deposit_money;
+void Bank_Account::To_Withdrawl(long long int z){
+cout<<"How much amount you want to withdraw: ";
+cin>>Withdraw_money;
+Bank_balance=Bank_balance-Withdraw_money;
+cout<<"Remaining Balance: "<<Bank_balance<<" Rs."<<endl;
 }
-
-void Bank_Account::To_Withdrawl(){
-    cout<<"How much amount you want to widrawl: ";
-    cin>>Withdrawl_money;
-    First_deposit_money+=Withdrawl_money;
-    cout<<"Total Balance: "<<First_deposit_money;
+void Bank_Account::Print_Pattern(){
+cout<<"************************************"<<endl;
 }
-void write_bank_account();
-void display(int n);
 int main()
 {
-    int option,num;
+    long long int num;
+    char option;
     Bank_Account b;
-
-cout<<"1). To create new account"<<endl
-    <<"2). To deposit money to your account"<<endl
-    <<"3). To withdraw money from your account"<<endl
-    <<"4). To Show your Account"<<endl;
-cout<<"What you want to do??"<<endl;
-cin>>option;
+b.Print_Pattern();
+cout<<"Hello..."<<endl
+    <<"Welcome To our Bank...."<<endl;
+b.Print_Pattern();
+cout<<endl;
+cout<<"1). create new account"<<endl
+    <<"2). deposit money to your account"<<endl
+    <<"3). withdraw money from your account"<<endl
+    <<"4). Show your Account"<<endl
+    <<"5). Delete account"<<endl
+    <<"6). Exit"<<endl;
+b.Print_Pattern();
+do{
+ cout<<"What you want to do??"<<endl;
+ cin>>option;
 switch(option)
 {
- case 1:
-      write_bank_account();
+ case '1':
+      system("CLS");
+      b.Print_Pattern();
+      b.To_create_acc();
+      b.Print_Pattern();
       break;
- case 2:
+ case '2':
+      system("CLS");
+      b.Print_Pattern();
       cout<<"Enter Bank account number: ";
       cin>>num;
-      b.Deposit();
+      if(num==b.AccNum){
+       b.deposit(num);
+      }
+      else{
+        cout<<"Error!! Please enter a valid account number... "<<endl;
+      }
+      b.Print_Pattern();
       break;
 
- case 3:
+ case '3':
+      system("CLS");
+      b.Print_Pattern();
       cout<<"Enter Bank account number: ";
       cin>>num;
-      b.To_Withdrawl();
-     break;
-
- case 4:
-    system("CLS");
-      cout<<"Enter Bank account number: ";
-      cin>>num;
-      display(num);
+      if(num==b.AccNum){
+       b.To_Withdrawl(num);
+      }
+      else{
+        cout<<"Error!! Please enter a valid account number... "<<endl;
+      }
+      b.Print_Pattern();
       break;
+
+ case '4':
+      system("CLS");
+      b.Print_Pattern();
+      cout<<"Enter Bank account number: ";
+      cin>>num;
+       if(num==b.AccNum){
+       b.Show_Acc(num);
+       }
+      else{
+        cout<<"Error!! Please enter a valid account number... "<<endl;
+       }
+       b.Print_Pattern();
+      break;
+
+ case '5':
+      system("CLS");
+////      Print_Pattern();
+      cout<<"Your account is successfully deleted.."<<endl;
+      b.Print_Pattern();
  default :
-    cout<<"\a";
-  }
+      cout<<"\a";
+}
+}while(option!='6');
+cout<<endl<<"Thanks for your trust and support..."<<endl;
+b.Print_Pattern();
 return 0;
-}
-void write_bank_account()
-{
-    Bank_Account ba;
-    ofstream file;
-    file.open("Account_Detail.txt", ios::app);
-    ba.To_create_acc();
-    file << "Name: " << NameAccHolder << endl;
-    file << "Account Type: " << AccType << endl;
-    file << "Balance: " << Bank_balance << endl;
-    //file << "Account number: " << ba.AccNum << endl;
-    file.close();
-}
-
-
-void display(int n){
-bool flag=false;
-    ifstream file;
-    file.open("Account_Detail.txt");
-    while(file>> n){
-        if(AccNum==n){
-            Show_Acc();
-            flag=true;
-        }
-    }
-    if(!flag){
-        cout<<"Account not found"<<endl;
-    }
-    file.close();
-}
-void deposit(int accountNumber, int amount) {
-bool found = false;
-    string line;
-    ifstream file("Account_Detail.txt");
-    ofstream temp("temp.txt");
-
-    while (getline(file, line)) {
-        if (line.find("Account number: " + to_string(accountNumber)) != string::npos) {
-            found = true;
-            int balance;
-            getline(file, line); // skip name
-            getline(file, line); // skip account type
-            getline(file, line); // read balance
-            balance = stoi(line.substr(line.find(": ") + 2));
-            balance += amount;
-            temp << "Name: " << NameAccHolder << endl;
-            temp << "Account Type: " << AccType << endl;
-            temp << "Balance: " << balance << endl;
-            temp << "Account number: " << accountNumber << endl;
-        }
-        else {
-            temp << line << endl;
-        }
-    }
-    file.close();
-    temp.close();
-
-    remove("Account_Detail.txt");
-    rename("temp.txt", "Account_Detail.txt");
-
-    if (found) {
-        cout << "Deposit successful!" << endl;
-    }
-    else {
-        cout << "Account not found." << endl;
-    }
 }
